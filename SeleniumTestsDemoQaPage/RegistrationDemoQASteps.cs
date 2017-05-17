fa7a3534dd49c4103292ffcb3b87908582b01a4b
vankatabe <ivan@vankata.be>
2017-05-17 15:45:06 +0300

    SpecFlow Gherkin two tests added for RegistrationDemoQA

---
+++ SeleniumTestsDemoQaPage/RegistrationDemoQASteps.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using SeleniumTestsDemoQaPage.Models;
+using SeleniumTestsDemoQaPage.Pages.RegistrationPage;
+using System;
+using System.Reflection;
+using TechTalk.SpecFlow;
+
+namespace SeleniumTestsDemoQaPage
+{
+    [Binding]
+    public class RegistrationDemoQASteps
+    {
+        IWebDriver driver = new FirefoxDriver();
+        private RegistrationUser user;
+        private RegistrationPage regPage;
+
+        [Given(@"I am on the Registration page")]
+        public void GivenIAmOnTheRegistrationPage()
+        {
+            regPage = new RegistrationPage(this.driver);
+            regPage.NavigateTo(regPage.URL);
+        }
+
+        [When(@"I fill-in the registration form without first name is ""(.*)""")]
+        public void WhenIFill_InTheRegistrationFormWithoutFirstNameIs(string testName)
+        {
+            user = AccessExcelData.GetTestUserData(testName);
+            regPage.FillRegistrationForm(user);
+        }
+
+        [Then(@"Error message for names should be displayed is ""(.*)""")]
+        public void ThenErrorMessageForNamesShouldBeDisplayedIs(string errorMessage)
+        {
+            MethodInfo asserter = typeof(RegistrationPageAssester).GetMethod(user.Asserter);
+            asserter.Invoke(null, new object[] { regPage, errorMessage });
+            // could be also like next row - Effect - from the Effect column in the Excel file. Need to add Effect property in RegistrationUser.cs
+            asserter.Invoke(null, new object[] { regPage, user.Effect });
+        }
+    }
+}
+
---
+++ SeleniumTestsDemoQaPage/SpecFlowTests/RegistrationDemoQA.feature
@@ -0,0 +1,23 @@
+﻿Feature: RegistrationDemoQA
+	In order to avoid silly mistakes
+	As a web idiot
+	I want to be told when I don't fill-in the form correctly
+	So I can fill it in correctly
+
+	or
+
+	As a User
+	I want to be able to fill-in the registration form
+	So I can get a registered account
+
+@mytag1
+Scenario: RegistrationWithoutFirstName
+	Given I am on the Registration page
+	When I fill-in the registration form without first name is "FirstNameField_Empty_ErrorMessage"
+	Then Error message for names should be displayed is "* This field is required"
+
+@mytag2
+Scenario: RegistrationWithoutLastName
+	Given I am on the Registration page
+	When I fill-in the registration form without first name is "LastNameField_Empty_ErrorMessage"
+	Then Error message for names should be displayed is "* This field is required"
